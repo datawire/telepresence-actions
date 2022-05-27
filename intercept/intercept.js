7bba8cbbbfbded05e8b0a42b697f50d569388f64
Victor Rojas <vicko16@gmail.com>
2022-05-27 13:58:03 -0500

    Print telepresence logs if the print_logs flag is enabled and skip the leave command when intercept returns an error

--- intercept/intercept.js
+++ intercept/intercept.js
@@ -12,6 +12,7 @@ const telepresenceIntercept = async function(){
         const ingress_port = core.getInput('ingress_port');
         const ingress_tls = core.getInput('ingress_tls');
         const ingress_l5 = core.getInput('ingress_l5');
+        const print_logs = core.getInput('print_logs');
         const parameters = ['intercept', service_name, '--port', service_port, '--ingress-host', ingress_host,
             '--ingress-port', ingress_port, '--ingress-l5', ingress_l5, '-n', namespace, `--http-header=${http_header}`];
         if (env_file && env_file.length !== 0){
@@ -20,7 +21,20 @@ const telepresenceIntercept = async function(){
         }
         if (ingress_tls)
             parameters.push('--ingress-tls')
+
+
         await exec.exec('telepresence', parameters);
+        core.saveState('telepresence_service_intercepted', true);
+
+        if (print_logs) {
+            await exec.exec('telepresence', ['gather-logs']);
+            await exec.exec('unzip', ['telepresence_logs.zip', '-d', 'intercept-logs']);
+            await exec.exec('cat', ['intercept-logs/cli.log']);
+            await exec.exec('cat', ['intercept-logs/connector.log']);
+            await exec.exec('cat', ['intercept-logs/daemon.log']);
+            const artifactClient = artifact.create();
+            await artifactClient.uploadArtifact('telepresence-logs', ['telepresence_logs.zip'], '.', {continueOnError: true})
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
