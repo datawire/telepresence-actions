3ad4d6c28860d298d8411b3f9bb7aac39f7aa1b7
Victor Rojas <vicko16@gmail.com>
2022-05-27 13:55:56 -0500

    Skip telepresence disconnect if connection was unsuccessful

--- connect/disconnect.js
+++ connect/disconnect.js
@@ -2,6 +2,12 @@ const core = require('@actions/core')
 const exec = require('@actions/exec');
 
 const telepresenceDisconnect = async function(){
+    const connected = core.getState('telepresence_connected');
+    if (!connected) {
+        core.notice("Skipping disconnect. Telepresence was unable to connect.");
+        return;
+    }
+
     try {
         await exec.exec('telepresence', ['quit']);
     } catch (error) {
