11dbe62a7c575869dfd203a59a380568663ba984
Keoni Delgado <keonidelgado@datawire.io>
2022-05-30 19:45:18 -0500

    Add TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION env var

--- login/login.js
+++ login/login.js
@@ -5,6 +5,8 @@ const telepresenceLogin = async function(){
     try {
         const API_KEY = core.getInput('telepresence_api_key');
         await exec.exec('telepresence', ['login', '--apikey', API_KEY]);
+        core.info(`TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION: 
+        ${process.env[TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION]}`);
     } catch (error) {
         core.setFailed(error.message);
     }
