6cec61be19157d3fa0b6ea0fef3cc4079b145bbc
Arturo Gonzalez Villa <42355868+arturogonzalez58@users.noreply.github.com>
2022-05-31 16:46:52 -0500

    Update src/configure/index.js
    Co-authored-by: Victor Rojas <vrojas-rendon@datawire.io>

--- src/configure/index.js
+++ src/configure/index.js
@@ -7,7 +7,7 @@ exports.getTelepresenceConfigPath = () => {
         case "darwin":
             return '/home/runner/Library/Application\\ Support/telepresence';
         case "linux":
-            return `/home/runner/.config/telepresence`;
+            return '/home/runner/.config/telepresence';
         default:
             core.setFailed(`The platform ${process.platform} is not supported yet`);
             return null;
