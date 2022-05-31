3dc0fc79ffeac079d469b2e91f7eb121ae178aa7
Keoni Delgado <keonidelgado@datawire.io>
2022-05-31 17:09:40 -0500

    Create the new MetritonClient

--- metrics/index.js
+++ metrics/index.js
@@ -1,18 +1,28 @@
-const reportDestination = 'https://metriton.datawire.io/beta/scout';
-const applicationName = 'telepresence-github-action-integration';
-const extensionVersion = '0.0.0-local';
+const HttpClient = require("@actions/http-client").HttpClient
 
-export function sendMetricsReport(action, metadata){
-    const payload = {
-        application: applicationName,
-        install_id: state.telepresenceStatus.installId,
-        version: extensionVersion,
-        metadata: {
-        ...(metadata || {}),
-        action,
-        },
-    };
-    window.ddClient.extension.vm.service.post(reportDestination, payload).catch(e => {
-        console.error('Error sending report:', e);
-    });
+
+class MetritonClient {
+    constructor() {
+        this.reportDestination = 'https://metriton.datawire.io/beta/scout';
+        this.applicationName = 'telepresence-github-action-integration';
+        this.installId = '0.0.0-local'
+        this.extensionVersion = '0.0.0-local';
+        this.httpClient = new HttpClient() 
+    }
+
+    async sendMetricsReport(action){
+        const payload = {
+            application: this.applicationName,
+            install_id: this.installId,
+            version: this.extensionVersion,
+            metadata: {
+            action,
+            },
+        };
+        httpClient.post(this.reportDestination, payload)
+    }
 }
+
+
+
+module.exports = MetritonClient
