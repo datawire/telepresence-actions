74c08315d281c8480001dcb20f79414f2646b067
Keoni Delgado <keonidelgado@datawire.io>
2022-05-30 19:58:01 -0500

    Fix

--- login/login.js
+++ login/login.js
@@ -6,7 +6,7 @@ const telepresenceLogin = async function(){
         const API_KEY = core.getInput('telepresence_api_key');
         await exec.exec('telepresence', ['login', '--apikey', API_KEY]);
         core.info(`TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION: 
-        ${process.env[TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION]}`);
+        ${process.env['TELEPRESENCE_REPORT_GITHUB_ACTION_INTEGRATION']}`);
     } catch (error) {
         core.setFailed(error.message);
     }
