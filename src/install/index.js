95dde43f460e10e4ee7768cd06cea90cf4f31862
Arturo Gonzalez Villa <arturogonzalez@datawire.io>
2022-06-01 10:52:27 -0500

    telepresence install cache fixed

--- src/install/index.js
+++ src/install/index.js
@@ -1,29 +1,33 @@
 const core = require('@actions/core');
 const toolCache = require('@actions/tool-cache');
+const cache = require('@actions/cache');
 const exec = require('@actions/exec');
+
+const TP_INSTALL_CACHE_ID = 'telepresence-install-id';
 
 const windowsInstall = async (version) => {
     core.setFailed('Not implemented for use with Windows runners');
 };
 
 const unixInstall = async  (version) => {
+    const cacheKey = TP_INSTALL_CACHE_ID + `-${version}`;
     const TELEPRESENCE_DOWNLOAD_URL = process.platform === 'darwin' ?
         `https://app.getambassador.io/download/tel2/darwin/amd64/${version}/telepresence` :
         `https://app.getambassador.io/download/tel2/linux/amd64/${version}/telepresence`;
 
-    const telepresenceBinary = toolCache.find('telepresence', version);
+    let tpCacheId = await cache.restoreCache([this.TP_PATH], cacheKey);
 
-    if (!telepresenceBinary) {
+    if (!tpCacheId) {
         try {
             await toolCache.downloadTool(TELEPRESENCE_DOWNLOAD_URL, `${this.TP_PATH}/telepresence`);
-            const cachedPath = await toolCache.cacheFile(`${this.TP_PATH}/telepresence`, 'telepresence', 'telepresence', version);
-            core.addPath(cachedPath);
+            tpCacheId = await cache.saveCache([this.TP_PATH], cacheKey);
+            if(!tpCacheId)
+                core.setFailed('There was a problem saving the telepresence binary.');
         } catch (e) {
-            core.setFailed(`There was a problem getting the telepresence binary: ${e}`)
+            core.setFailed(`There was a problem getting the telepresence binary: ${e}`);
         }
-    } else {
-        core.addPath(telepresenceBinary);
     }
+    core.addPath(this.TP_PATH);
     await exec.exec("chmod", ['a+x', `${this.TP_PATH}/telepresence`]);
 };
 
