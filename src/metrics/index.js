fb321a3560f8773b1dd3663b78f09e4c8bf6271d
Keoni Delgado <keonidelgado@datawire.io>
2022-06-01 19:35:44 -0500

    get runner install id

--- src/metrics/index.js
+++ src/metrics/index.js
@@ -1,11 +1,16 @@
+const fs = require('fs/promises');
 const HttpClient = require("@actions/http-client").HttpClient
+const io = require('@actions/io');
+const installTelepresence = require('../install');
 
 const httpClient = new HttpClient()
 const reportDestination = 'https://metriton.datawire.io/beta/scout'
 const applicationName = 'telepresence-github-action-integration'
-const installId = '0.0.0-local'
+const installId = await fs.readFile(installTelepresence.getTelepresenceConfigPath())
 const extensionVersion = '0.0.0-local'
 const pipelineId = process.env.GITHUB_RUN_ID
+
+console.log(installId)
 
 class MetritonClient {
     static sendMetricsReport(action){
