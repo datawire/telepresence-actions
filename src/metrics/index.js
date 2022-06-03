34520e65122ee2c1dc3781217d532abab648ae7b
Keoni Delgado <keonidelgado@datawire.io>
2022-06-03 18:39:05 -0500

    fix

--- src/metrics/index.js
+++ src/metrics/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const HttpClient = require("@actions/http-client").HttpClient
 const AmbassadorClient = require('../ambassador/api-client');
 const core = require('@actions/core');
+const configure = require('../configure');
 
 const httpClient = new HttpClient();
 const ambassadorClient = new AmbassadorClient(process.env.TELEPRESENCE_API_KEY)
@@ -11,7 +12,7 @@ class MetritonClient {
         try {
             const reportDestination = 'https://metriton.datawire.io/beta/scout'
             const applicationName = 'telepresence-github-action-integration'
-            const installId = fs.readFileSync(`${configureTP.getTelepresenceConfigPath()}/id`).toString()
+            const installId = fs.readFileSync(`${configure.getTelepresenceConfigPath()}/id`).toString()
             core.info('install id: ' + installId)
             const extensionVersion = '0.0.0-local'
             const pipelineId = process.env.GITHUB_RUN_ID
