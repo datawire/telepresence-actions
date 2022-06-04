949c792c67d53667d0d6f277a32011258c84e747
Keoni Delgado <keonidelgado@datawire.io>
2022-06-03 19:09:53 -0500

    fix

--- connect/connect.js
+++ connect/connect.js
@@ -10,6 +10,7 @@ const telepresenceConnect = async function(){
     try {
         MetritonClient.sendMetricsReport('connect')
         await exec.exec('telepresence', ['connect']);
+        core.info('TELEPRESENCE_GITHUB_ACTION_INTEGRATION' + process.env["TELEPRESENCE_GITHUB_ACTION_INTEGRATION"])
         core.saveState("telepresence_connected", true)
     } catch (error) {
         core.setFailed(error.message);
