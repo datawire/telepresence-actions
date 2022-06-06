9049c5bdc33092dd34b7fc2d6a0101d51ef26a55
Keoni Delgado <keonidelgado@datawire.io>
2022-06-06 11:20:25 -0500

    Fix echo add telepresence report

--- install/install.js
+++ install/install.js
@@ -1,5 +1,5 @@
 const install = require('../src/install');
 const exec = require('@actions/exec')
 
-exec.exec('echo "TELEPRESENCE_REPORT_GITHUB_ACTIONS_INTEGRATION=true" >> $GITHUB_ENV')
+exec.exec('echo', ['"TELEPRESENCE_REPORT_GITHUB_ACTIONS_INTEGRATION=true" >> $GITHUB_ENV'])
 install.telepresenceInstall();
