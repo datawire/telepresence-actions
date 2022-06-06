e658098ded7b7aadbfaa4bf468f6e51d3b9e21d2
Keoni Delgado <keonidelgado@datawire.io>
2022-06-06 09:30:26 -0500

    Add TELEPRESENCE_GITHUB_ACTION_INTEGRATION=true

--- install/install.js
+++ install/install.js
@@ -1,3 +1,6 @@
 const install = require('../src/install');
+const exec = require('@actions/exec')
 
+
+exec.exec('echo "TELEPRESENCE_GITHUB_ACTION_INTEGRATION=true" >> $GITHUB_ENV')
 install.telepresenceInstall();
