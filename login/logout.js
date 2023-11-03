727a71c273a7191c4082fb2b443bd55ee4a0073a
njayp <nickjaypowell@gmail.com>
2023-11-03 02:00:46 -0700

    wait for demo app
    Signed-off-by: njayp <nickjaypowell@gmail.com>

--- login/logout.js
+++ login/logout.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const MetritonClient = require('../src/metrics');
+//const MetritonClient = require('../src/metrics');
 
 const telepresenceLogout = async function () {
   const sessionCreated = core.getState('telepresence_session_created');
@@ -10,7 +10,7 @@ const telepresenceLogout = async function () {
   }
 
   try {
-    MetritonClient.sendMetricsReport('logout');
+    //MetritonClient.sendMetricsReport('logout');
     await exec.exec('telepresence', ['logout']);
   } catch (error) {
     core.setFailed(error.message);
